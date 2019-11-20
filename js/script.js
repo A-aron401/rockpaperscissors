1671c8551d72ce505c0fd09ec76410be2dee9208
aaliyah joseph <s_aaliyah.joseph@ousd.org>
2019-11-20 02:31:34 +0000

    I set up the computer choice and the user choice.

--- js/script.js
+++ js/script.js
@@ -4,13 +4,35 @@
 
 //GLOBAL VARIABLES
 /* global $ */
+var computerChoice;
+var randomNumber;
+var winner;
 $("#shoot").click(function() {
 var userChoice= $("#input").val();
 console.log("checking: " + userChoice);
  $("#userChoice").text(userChoice);
 
-console.log("text");
+//console.log("text");
+randomNumber = Math.ceil(Math.random()*3);
+if (randomNumber=== 3) {
+    $("#computerChoice").text("rock");
+} else if(randomNumber=== 2) {
+    $("#computerChoice").text("paper");
+} else {
+    $("#computerChoice").text("scissors")
+}
+if ((userChoice=== "paper"&& computerChoice==="rock")|| (userChoice==="rock"&& computerChoice=== "scissors")|| (userChoice=== "scissors"&& computerChoice=== "paper")) {
+ $("#result").text("Winner");
+} else if((userChoice=== "paper"&& computerChoice=== "paper")|| (userChoice==="rock"&& computerChoice=== "rock")|| (userChoice==="scissors"&& computerChoice=== "scissors")){
+    $("#result").text("Tied");
+} else if ((userChoice=== "rock"&& computerChoice=== "paper")|| (userChoice=== "scissors"&& computerChoice=== "rock")|| (userChoice=== "paper"&& computerChoice=== "scissors")){
+    $("#result").text("Lose");
+} else{
+    $("#result").text("Debugging");
+}
+console.log("hi");
 });
+
 
 // DOCUMENT READY FUNCTION BELOW
 
